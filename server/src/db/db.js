5682ab11a24a368ec7ae99931b07559588828a49
Naved Uddin <naveduddin83@gmail.com>
2024-05-03 13:02:20 +0530

    db connection from atlas

--- server/src/db/db.js
+++ server/src/db/db.js
@@ -1,14 +1,14 @@
 import mongoose from 'mongoose';
 
-const DB_URI = 'mongodb://127.0.0.1:27017';
-const DB_NAME = 'alphasystem_dev';
+const DB_URI = process.env.DB_URI || 'mongodb://127.0.0.1:27017';
+const DB_NAME = process.env.DB_NAME || 'alphasystem_dev';
 
 export const connectDB = async () => {
   try {
-      await mongoose.connect(`${DB_URI}/${DB_NAME}`);
-      console.log('Database connected !!!');
+    await mongoose.connect(`${DB_URI}/${DB_NAME}`);
+    console.log('Database connected !!!');
   } catch (error) {
-      console.error(`\n MongoDB Connection Error : ${error}`);
-      process.exit(1);
+    console.error(`\n MongoDB Connection Error : ${error}`);
+    process.exit(1);
   }
 }
