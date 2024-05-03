b73c0e1a4d3c06af52b81b00f7d5d4cc9d04e07f
Naved Uddin <naveduddin83@gmail.com>
2024-05-03 18:39:21 +0530

    user list functioning working

--- client/admin/js/dashboard.js
+++ client/admin/js/dashboard.js
@@ -16,3 +16,39 @@ logoutBtn.addEventListener('click', (e) => {
     window.location.href = '../index.html';
 });
 
+
+// Fetch users data from the backend
+const getUsers = async () => {
+    try {
+        const table = document.querySelector('tbody');
+        const response = await fetch('http://localhost:3000/api/users');
+        if (!response.ok) {
+            throw new Error('Failed to fetch users');
+        }
+        const users = await response.json();
+        
+        for(let i=0; i<users.length; i++) {
+            let name = users[i].fullname ?? 'Unknown';
+            let user = users[i].username ?? 'Unknown';
+            let level = users[i].level ?? 1;
+            let status = users[i].status ?? 'Inactive';
+            let edit = `<i class="fa-solid fa-pen-to-square"></i>`;
+            let remove = `<i class="fa-solid fa-ban"></i>`;
+            
+            table.innerHTML += `
+                <tr>
+                    <td>${name}</td>
+                    <td>${user}</td>
+                    <td>${level}</td>
+                    <td>${status}</td>
+                    <td>${edit} ${remove}</td>
+                </tr>
+            `;
+        }
+    } catch (error) {
+        console.error('Error fetching users:', error);
+    }
+};
+
+// Call the fetchUsers function to initiate the fetch operation
+getUsers();
