8fbeb56803b7425d4c7b5d7728920313a77c7b65
Naved Uddin <naveduddin83@gmail.com>
2024-05-03 18:17:17 +0530

    admin dashboard working

--- client/admin/js/dashboard.js
+++ client/admin/js/dashboard.js
@@ -8,9 +8,11 @@ if(!isLogin()) {
     window.location.href = '../index.html';
 }
 
+// Logout logic
 logoutBtn.addEventListener('click', (e) => {
     e.preventDefault();
     deleteCookie('username');
     deleteCookie('password');
     window.location.href = '../index.html';
 });
+
